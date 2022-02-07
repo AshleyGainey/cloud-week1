2216e5eaf55cf8ea2dc97ffaad8b0565cad0c827
AshleyGainey <ashleygaineywork@gmail.com>
2022-02-07 11:40:35 +0000

    Week3 Package

--- test.js
+++ test.js
@@ -5,9 +5,6 @@ const port = 3000
 app.get('/', (req, res) => {
  res.send('Hello Virtual Machine!')
 })
-app.get('/', (req, res) => {
-    res.send('Heee')
-   })
 app.listen(port, () => {
  console.log(`Express Application listening at port 3000`)
 })
